0766982e03f2fb607c200012fc83f49b942e8469
manvfx <manvfx@gmail.com>
2024-06-22 21:42:49 +0330

    add swagger

--- server.mjs
+++ server.mjs
@@ -10,24 +10,80 @@ import { bullPlugin } from "./app/plugins/bull.mjs";
 
 const fastify = Fastify({ logger: true });
 
+//setup swagger
+import swagger from "@fastify/swagger";
+import swaggerUi from "@fastify/swagger-ui";
+
+await fastify.register(swagger, {
+  swagger: {
+    info: {
+      title: "Super Backend API",
+      description: "API documentation",
+      version: "1.0.0",
+    },
+  },
+});
+await fastify.register(swaggerUi, {
+  routePrefix: "/documentation",
+  theme: {
+    title: "Super Api docs",
+  },
+  uiConfig: {
+    docExpansion: "full",
+    deepLinking: false,
+  },
+  uiHooks: {
+    onRequest: function (request, reply, next) {
+      next();
+    },
+    preHandler: function (request, reply, next) {
+      next();
+    },
+  },
+  staticCSP: true,
+  transformStaticCSP: (header) => header,
+  transformSpecification: (swaggerObject, request, reply) => {
+    return swaggerObject;
+  },
+  transformSpecificationClone: true,
+});
+
 fastify.register(nanoidPlugin);
 fastify.register(redisPlugin);
-fastify.register(mongodbPlugin, { mongoUrl: config.mongoUrl, dbName: config.dbName });
+fastify.register(mongodbPlugin, {
+  mongoUrl: config.mongoUrl,
+  dbName: config.dbName,
+});
 fastify.register(bullPlugin);
 fastify.register(authPlugin);
 
-fastify.get('/', async (request, reply) => {
-    return await reply.send({ "Super Rest API": "Works fine!" });
-})
-
+fastify.get("/", {
+  schema: {
+    description: "Home Endpoint",
+    tags: ["Default"],
+    summary: "An home endpoint",
+    response: {
+      200: {
+        description: "Successful response",
+        type: "object",
+        properties: {
+          hello: { type: "string" },
+        },
+      },
+    },
+  },
+  handler: async (request, reply) => {
+    return { hello: "world" };
+  },
+});
 
 export async function start() {
-    try {
-        fastify.listen({ port: config.server_port, host: config.server_host });
-    } catch (err) {
-        fastify.log.error(err);
-        process.exit(1);
-    }
+  try {
+    fastify.listen({ port: config.server_port, host: config.server_host });
+    await fastify.ready();
+    fastify.swagger();
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
 }
-
-
